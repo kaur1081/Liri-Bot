d306da00a7f96cdf8a13dee4bf8c71f233cc4052
kaur1081 <karishmawalia@gmail.com>
2019-08-30 21:41:00 -0700

    not working

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ Include screenshots, gifs or videos of the app functioning
 Contain a link to a deployed version of the app
 Clearly list the technologies used in the app
 State your role in the app development
+
+// var spotify = new Spotify(keys.spotify);
+//  code to read and set any environment variables with the dotenv package
+// code required to import the keys.js file and store it in a variable.
+//  to access your keys information 
+// Import the request npm package.
+// Make it so liri.js can take in one of the following commands:
--- liri.js
+++ liri.js
@@ -1,18 +1,12 @@
 require("dotenv").config();
-//  code to read and set any environment variables with the dotenv package
-// code required to import the keys.js file and store it in a variable.
-var keys = require("./keys.js");
+var keys    = require("./keys.js");
 var Spotify = require("node-spotify-api");
-// var spotify = new Spotify(keys.spotify);
-var moment =require('moment');
-//  to access your keys information 
-// Import the request npm package.
-var axios = require('axios');
+var moment  = require('moment');
+var axios   = require('axios');
 var request = require("request");
-var fs =require('fs');
-// Make it so liri.js can take in one of the following commands:
+var fs      = require('fs');
+// --------------------------------------------------------
 // concert-this
-
 function concertThis(band) {
    axios.get("https://rest.bandsintown.com/artists/" + band + "/events?app_id=codingbootcamp").then(
            function (res) {
@@ -38,72 +32,78 @@ function concertThis(band) {
 
 
 // spotify-this-song
+// -----------------------------------------------------
 
-
-
-
-
-
-function spotify(songs){
-  // spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  //   if (err) {
-  //     return console.log('Error occurred: ' + err);
-  //   }
-  //  console.log(data); 
-  // });
-  
-
-// ----------------------------------------
-
-var jsonData = data.tracks.items[0];
-
-var songData = [
-    "-----------------------------------",
-    "ARTIST: " + jsonData.artists[0].name,
-    "SONG NAME: " + jsonData.name,
-    "ALBUM " + jsonData.album.name,
-    "PREVIEW LINK: " + jsonData.external_urls.spotify,
-    "------------------------------------",
-].join("\n\n");
-console.log(songData);
+var getArtistsName = function(artist){
+  return artist.name;
 }
 
-// --------------------------------------------------
-// for(var i=0; i<songs.length; i++){
-//     console.log(i);
-//     console.log('artist(s):' + songs[i].artists.map(getArtistsName));
-//     console.log('song name:' + songs[i].name);
-//     console.log('preview songs: ' + songs[i].preview_url);
-//     console.timeLog('album: ' + songs[i].album.name);
-//     console.log('----------------------------------------');
-// }
 
+var getMeSpotify =  function(songName){
+  spotify.search({ type: 'track', query: 'songName'}, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+  var songs = data.tracks.items[0]; 
+  for(var i=0; i<songs.length; i++){
+    console.log(i);
+    console.log('artist(s):' + songs[i].artists.map(getArtistsName));
+    console.log('song name:' + songs[i].name);
+    console.log('preview songs: ' + songs[i].preview_url);
+    console.timeLog('album: ' + songs[i].album.name);
+    console.log('----------------------------------------');
+}
+  });
+};
   // area for movie
-  function movieThis(){
+  var getTheMovie = function (movieThis){
+  var URL = "http://www.omdbapi.com/?t=" + movieThis + "&y=&plot=short&apikey=trilogy";
 
-  }
+  axios.get(URL).then(function(response) {
+      var movieData = response.data
+      console.log("-------------------------------\n");
+      console.log("MOVIE TITLE: " + movieData.Title + '\n');
+      console.log("RELEASE: " + movieData.Year + '\n');
+      console.log("IMDB RATING: " + movieData.Ratings[0].Value + '\n');
+      console.log("ROTTEN TOMATOES RATING: " + movieData.Ratings[1].Value + '\n');
+      console.log("COUNTRY: " + movieData.Country + '\n');
+      console.log("LANGUAGE: " + movieData.Language + '\n');
+      console.log("PLOT: " + movieData.Plot + '\n');
+      console.log("ACTORS: " + movieData.Actors + '\n');
+      console.log("-------------------------------");
+  })
+  .catch(function(error) {
+      if (error.response){
+          console.log(error);
+      };
+  });
+};
+ // create do-what-it-says
 
-  // create do-what-it-says
 function whatItSays(ask, display){
+  
   switch(ask){
+
     case "concert-this":
-      console.log("band search")
-      concertThis(display)
+      // console.log("band search");
+      concertThis(display);
       break;
+
       case "spotify":
-      console.log("")
-     spotify(display)
+      console.log("song-search");
+      getMeSpotify();
       break;
+
     case "movie-this":
-      console.log("movie search")
-      //movie search function
+      console.log("movie search");
+      getTheMovie(movieData);
       break;
   }
-
-}
-
+};
+// function to run this
 function runThis(argOne, argTwo){
-    whatItSays(argOne, argTwo)
+    whatItSays(argOne, argTwo);
 }
-
-runThis(process.argv[2],process.argv[3])
+  
+  
+runThis(process.argv[2],process.argv[3]);
